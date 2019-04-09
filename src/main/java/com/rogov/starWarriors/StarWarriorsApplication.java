901f4c1278cb49edec42f065a3989868a9078c4f
AlexandrRogov <rohaualexandr@gmail.com>
2019-04-10 02:51:04 +0300

    First commit

---
+++ src/main/java/com/rogov/starWarriors/StarWarriorsApplication.java
@@ -0,0 +1,13 @@
+package com.rogov.starWarriors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarriorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarWarriorsApplication.class, args);
+    }
+
+}
